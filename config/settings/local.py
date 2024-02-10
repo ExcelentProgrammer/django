869082c5b0f490d343f8d838eac748b44a1f0ab5
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-10 18:41:45 +0500

    update requirements

---
+++ config/settings/local.py
@@ -0,0 +1,44 @@
+from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+####################
+# DEBUG BAR
+####################
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+#####################
+# Allowed Hosts
+#####################
+ALLOWED_HOSTS += [
+    "127.0.0.1",
+    "192.168.100.26"
+]
