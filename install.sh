2f6f50461edcdabc960dd2ec4a201b357c90c137
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-12 12:26:51 +0500

    update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ virtualenv venv
 pip install -r ./requirements/local.txt
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
+
+echo "DJANGO SECRET KEY: "
+python3 ./manage.py generate_secret_key
