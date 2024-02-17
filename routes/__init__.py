e8383aff89e5a2200ee9133e62763080ecd2358c
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-17 17:24:42 +0500

    update

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 
 from routes.media import media_urls
-from routes.swagger import swagger_urls
 from .index import urlpatterns
-from .local import local_urls
 
 urlpatterns += media_urls
 
 if settings.DEBUG:
+    from .local import local_urls
+    from routes.swagger import swagger_urls
+
     urlpatterns += swagger_urls
     urlpatterns += local_urls
