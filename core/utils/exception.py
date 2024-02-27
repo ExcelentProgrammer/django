80ddfa65c426cf4cd0d426d5abf377a5c975dc93
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-02-27 13:01:13 +0500

    update

--- core/utils/exception.py
+++ core/utils/exception.py
@@ -6,7 +6,7 @@ from core.exceptions import BreakException, MyApiException
 class ResponseException:
 
     def __init__(self, message="", data=None, error_code=0,
-                 status_code=status.HTTP_400_BAD_REQUEST, exception=None):
+                 status_code=status.HTTP_400_BAD_REQUEST, exception=None, **kwargs):
         if isinstance(exception, BreakException):
             raise exception
 
@@ -16,6 +16,7 @@ class ResponseException:
             "success": False,
             "message": message,
             "data": data,
-            "error_code": error_code
+            "error_code": error_code,
+            **kwargs
         }
         raise MyApiException(response, status_code)
--- core/utils/response.py
+++ core/utils/response.py
@@ -19,7 +19,7 @@ class ApiResponse:
 
     @staticmethod
     def error(message="", data=None, error_code=0,
-              status_code=status.HTTP_400_BAD_REQUEST, exception=None):
+              status_code=status.HTTP_400_BAD_REQUEST, exception=None, **kwargs):
 
         #####################
         # Ignore BreakException
@@ -33,6 +33,7 @@ class ApiResponse:
             "success": False,
             "message": message,
             "data": data,
-            "error_code": error_code
+            "error_code": error_code,
+            **kwargs
         }
         raise MyApiException(response, status_code)
