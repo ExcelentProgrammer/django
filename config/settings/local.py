5e55d8180efc4e0974bd0e3653f75239f1228f88
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-03-06 16:02:06 +0500

    update

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,8 +3,11 @@ from ..conf.rest_framework import REST_FRAMEWORK
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
     }
 }
 
