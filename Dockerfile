b527254745a4b1fd7f547a7dcbafa3ad75cb0336
Azamov Samandar <azamov.samandar.programmer@gmail.com>
2024-03-19 11:37:26 +0500

    create polymorphic relation ship

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.11-slim
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /code
+
+
+COPY . /code/
+
+RUN pip install -r requirements/local.txt
+
--- config/conf/cache.py
+++ config/conf/cache.py
@@ -3,7 +3,7 @@ from common.env import env
 CACHES = {
     'default': {
         'BACKEND': env("CACHE_BACKEND"),
-        'LOCATION': env("CACHE_LOCATION"),
+        'LOCATION': env("REDIS_URL"),
         "TIMEOUT": env("CACHE_TIMEOUT")
     },
 }
